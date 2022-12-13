fe5abf72c3cc6fa994d22dec54e4047fe6c9ebd5
armaBurton <armanarma@armaburton.com>
2022-12-12 17:16:14 -0800

    delete

--- src/items/items.router.ts
+++ src/items/items.router.ts
@@ -75,3 +75,14 @@ itemsRouter.put("/:id", async (req: Request, res: Response) => {
 });
 
 // DELETE items/:id
+itemsRouter.delete("/:id", async (req: Request, res: Response) => {
+  try {
+    const id: number = parseInt(req.params.id, 10);
+
+    await ItemService.remove(id);
+
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
