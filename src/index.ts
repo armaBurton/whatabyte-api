cba5a0ce55198150f25813f8b851a857d7a1b990
armaBurton <armanarma@armaburton.com>
2022-12-12 16:32:41 -0800

    nodemon setup

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,26 @@ dotenv.config();
 /**
  * App Variables
  */
+!process.env.PORT && process.exit(1);
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+const app = express();
 
 /**
  *  App Configuration
  */
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(`<h1>Nodemon</h1>`);
+});
 
 /**
  * Server Activation
  */
+app.listen(PORT, () => {
+  console.log(`*** Listening on port ${PORT} ***`);
+});
