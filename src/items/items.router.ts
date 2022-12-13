01d23d737fa3d68b11acfce68b6d0e6ff79c25a5
armaBurton <armanarma@armaburton.com>
2022-12-12 16:58:12 -0800

    router modules

---
+++ src/items/items.router.ts
@@ -0,0 +1,24 @@
+/**
+ * Required External Modules and Interfaces
+ */
+import express, { Request, Response } from "express";
+import * as ItemService from "./items.service";
+import { BaseItem, Item } from "./item.interface";
+
+/**
+ * Router Definition
+ */
+
+/**
+ * Controller Definitions
+ */
+
+// GET items
+
+// GET items/:id
+
+// POST items
+
+// PUT items/:id
+
+// DELETE items/:id
